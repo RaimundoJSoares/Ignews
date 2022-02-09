3a84d13580ed6d01ef5ddb58313a3823495842c8
RaimundoJSoares <raianimes1000@gmail.com>
2022-02-09 17:46:50 -0400

    Desenvolvendo a Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
+import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+
 export default function Home() {
   return (
-    <h1>Hello World, I will be the best one</h1>
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+    <main>
+      <section>
+        <span>👏 Hey, Welcome</span>
+        <h1>News about the <span>React</span>World</h1>
+        <p>Get the access to all publications <br/>
+        <span>for $5.00 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt='avatar' />
+      <SubscribeButton/>
+    </main>
+    </>
   )
 }
