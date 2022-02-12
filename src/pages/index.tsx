d4699a201c06b407e705b9707f825823c5c05d98
RaimundoJSoares <raianimes1000@gmail.com>
2022-02-11 22:01:48 -0400

    autenticando com github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { stripe } from '../components/services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -31,7 +31,7 @@ export default function Home({product}: HomeProps ) {
     </>
   )
 }
-export const getServerSideProps : GetServerSideProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
   const  price = await stripe.prices.retrieve('price_1KRksYKs7Tu9g7COghBJk3ii')
 
     const product = {
@@ -45,6 +45,6 @@ export const getServerSideProps : GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    }, revalidate : 60 * 60 * 24  //24 horas
   }
 }
