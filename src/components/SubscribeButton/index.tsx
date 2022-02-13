f1ce4d1c0a0403054ce4f614c2c950c0fa44fab5
RaimundoJSoares <raianimes1000@gmail.com>
2022-02-13 18:50:43 -0400

    implementando stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss'
 
 interface SubscribeButton {
@@ -5,10 +6,22 @@ interface SubscribeButton {
 }
 
 export function SubscribeButton({priceId}: SubscribeButton) {
+  const { data: session } = useSession();
+
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    //se estiver logado, faremos a criação da logou session
+}
   return(
     <button
     type='button'
     className={styles.subscribeButton}
+    onClick={h}
     >
       Subscribe Now
     </button>
